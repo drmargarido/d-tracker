c34d0f4aa2427889bf006094e81b53244b791d85
Daniel Margarido <drmargarido@gmail.com>
2019-12-08 20:35:00 +0000

    Implemented escaping in the xml export so the created xml files are valid.

--- src/exporter/xml.lua
+++ src/exporter/xml.lua
@@ -16,6 +16,27 @@ local activity_template = [[
     />
 ]]
 
+local xml_escaped_chars = {
+    ['"'] = "&quot;",
+    ["'"] = "&apos;",
+    ["<"] = "&lt;",
+    [">"] = "&gt;",
+    ["&"] = "&amp;"
+}
+
+local escape = function(attribute)
+    local escaped_attribute = ""
+    for ch in attribute:gmatch(".") do
+        if xml_escaped_chars[ch] then
+            escaped_attribute=escaped_attribute..xml_escaped_chars[ch]
+        else
+            escaped_attribute=escaped_attribute..ch
+        end
+    end
+
+    return escaped_attribute
+end
+
 return function(tasks, file_path)
     local tasks_activities = ""
 
@@ -26,8 +47,8 @@ return function(tasks, file_path)
 
         tasks_activities=tasks_activities..string.format(
             activity_template,
-            task.project,
-            task.description,
+            escape(task.project),
+            escape(task.description),
             duration:spanminutes(),
             start_time:fmt("%Y-%m-%d %H:%M:%S"),
             end_time:fmt("%Y-%m-%d %H:%M:%S")
