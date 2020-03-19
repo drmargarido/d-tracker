dee4315cd0849b0286a8066c46b36339f014d684
dmargarido <dmargarido@dognaedis.com>
2020-03-19 00:46:07 +0000

    Added new controller to list the existing projects. Implemented the command in the cli to list the existing projects.

--- src/cli/commands/list_projects.lua
+++ src/cli/commands/list_projects.lua
@@ -1,8 +1,14 @@
+local list_projects = require "src.controller.list_projects"
+
 return function(parser)
     parser
         :command("list-projects")
         :summary("Lists the projects available in the database")
         :action(function(args, name)
+            local projects = list_projects()
 
+            for _, project in ipairs(projects) do
+                print(string.format("%d|%s", project.id, project.name))
+            end
         end)
 end
---
+++ src/controller/list_projects.lua
@@ -0,0 +1,18 @@
+-- Validators
+local validators = require "src.validators.base_validators"
+
+-- Decorators
+local decorators = require "src.decorators"
+local use_db = decorators.use_db
+
+return use_db(function(db)
+    local projects = {}
+    for row in db:nrows("SELECT * FROM project") do
+        table.insert(projects, {
+            id=row.id,
+            name=row.name
+        })
+    end
+
+    return projects
+end)
