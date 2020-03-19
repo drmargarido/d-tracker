e708afda2e4e3104e49805429fd5477548d17a40
dmargarido <dmargarido@dognaedis.com>
2020-03-19 02:19:24 +0000

    Implemented cli command to add a new task to d-tracker. Added cli command to stop the current task in progress.

--- src/cli/commands/stop_in_progress.lua
+++ src/cli/commands/stop_in_progress.lua
@@ -1,8 +1,16 @@
+-- Controllers
+local stop_task = require "src.controller.stop_task"
+
 return function(parser)
     parser
         :command("stop-in-progress")
         :summary("Stop the current task in progress")
         :action(function(args, name)
+            local _, err = stop_task()
+            if err then
+                print(err)
+            end
 
+            return
         end)
 end
